ecc228290746f57713de2d09bc5dee3594175a99
LeeQI <451117877@QQ.COM>
2016-11-24 20:29:25 +0800

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,23 +1,52 @@
 <?php
 //增，删，改，查。
+try {
+				$dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+				$stmt = $dbh->prepare($s);
+				
+				if($stmt->execute())
+					//echo 'success';
+					Log::DEBUG("create success:".$s);
+				else
+				   //echo 'fail';
+					Log::DEBUG("create fail:".$s);				   
+	    
+				$dbh = null;
+			} catch (PDOException $e) 
+			{
+				 Log::DEBUG("Error!: " . $e->getMessage());
+				//die();
+			}
+//专业搞事情。。。
+
+
+
+
+
+
+
+
+
+
 //字符串的生成
-$data= array();
-$data['ss']=1;
-$data['sss']="aaa";
-
-
-$s="inserrt into`JingyueWechatPay` (";
-$s1="";
-$s2="values(";
-$i=1;
-foreach($data as $key => $value)
+			   	$s="insert into `JingyueWechatPay` (";
+				$s1="";
+				$s2="values(";
+				$i=1;
+		foreach($data as $key => $value)
 	   {	
+			if(!is_int($value))
+			{
+				$s1.="$key,";
+				$s2.="'$value',";
+			}
+			else
+			{
+				$s1.="$key,";
+				$s2.="$value,";
+			}
 			
-			$s1.="$key,";
-            $s2.="?,";
-    // $stmt->bindParam($i, $value);
-    		$i++;
-			
+    		
 	   }
 $s1=trim($s1,",");
 	   $s1.=")";
@@ -25,5 +54,27 @@ $s2=trim($s2,",");
 	   $s2.=")";
 
 $s.=$s1." ".$s2;
-echo $s;
 //  专门为数组插入table准备。
+
+
+
+$s="create table `JingyueWechatPay` (";
+	   foreach($data as $key => $value)
+	   {	
+		
+			
+			if(!is_int($value))
+			{
+				$s.="$key varchat(128),";
+			}
+			else
+			{
+				$s.="$key int,";
+			}
+	   }
+	   $s=trim($s,",");
+	   $s.=")";
+		$user="root";
+		$pass=123456;
+		
+// 专业建表
